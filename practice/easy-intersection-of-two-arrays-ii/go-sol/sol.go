ceb773ef8a446107056175ccb3e2ae1c0ac3121a
Phan Phú Thành <thanhphanphu18@gmail.com>
2024-04-17 14:54:35 +0700

    fix: easy-intersection-of-two-arrays-ii

--- practice/easy-intersection-of-two-arrays-ii/go-sol/sol.go
+++ practice/easy-intersection-of-two-arrays-ii/go-sol/sol.go
@@ -1,22 +1,28 @@
 package gosol
 
 func intersect(nums1 []int, nums2 []int) []int {
-	res := []int{}
-	m := make(map[int]int)
+    var (
+        nums1Count = make(map[int]int)
+        nums2Count = make(map[int]int)
+    )
+    for _, n := range nums1 {
+        nums1Count[n] += 1
+    }
+    for _, n := range nums2 {
+        nums2Count[n] += 1
+    }
 
-	for _, num := range nums1 {
-		m[num]++
-	}
+    var intersect []int
 
-	for _, num := range nums2 {
-		if counter, ok := m[num]; ok && counter > 0 {
-			m[num]--
-			res = append(res, num)
-            if len(res) == len(nums1) {
-                return res
-            }
-		}
-	}
+    for n, c1 := range nums1Count {
+        c2, ok := nums2Count[n]
+        if !ok {
+            continue
+        }
+        for i := 0; i < min(c1, c2); i++ {
+            intersect = append(intersect, n)
+        }
+    }
 
-	return res
+    return intersect
 }
